320ce05deeec1a936d5d9b9aebf47183db7ec40c
Oliviya11 <nastyawnastya@gmail.com>
2017-06-05 15:59:14 +0300

    Menu scene

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayButton : MonoBehaviour {
+
+	public UnityEvent signalOnClick = new UnityEvent();
+
+
+	public void _onClick() {
+		this.signalOnClick.Invoke ();
+	}
+}
